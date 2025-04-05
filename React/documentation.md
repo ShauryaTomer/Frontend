a3e0a73e3b81f709123284ec093c63e27ea61e4c
Shaurya <shauryatomer10@gmail.com>
2025-04-05 22:21:35 +0530

    react initial-introduction files

---
+++ React/documentation.md
@@ -0,0 +1,29 @@
+# React
+
+## Introduction Slides
+
+![image](https://i.imgur.com/J3qfc3s.png)
+![image](https://i.imgur.com/j4D8pDX.png)
+![image](https://i.imgur.com/AH6Xa9A.png)
+![image](https://i.imgur.com/qYgq4wW.png)
+![image](https://i.imgur.com/d1l61tV.png)
+![image](https://i.imgur.com/muRXNqL.png)
+![image](https://i.imgur.com/fWu1ZdY.png)
+![image](https://i.imgur.com/D8N20j5.png)
+
+### Initial Setup
+
+- `npm create vite@latest <project-name>` : Create a new React project
+- `npm install` : Install all dependencies
+- `npm run dev` : Run the project
+- `vite.config.js` : Define the project configuration
+- `index.html` : Define the project entry point
+- `main.jsx` : Define the project entry point
+- Delete app.css because we don't need it as we will use Tailwind CSS. Also clear App.jsx
+- Download "ES7 + React/Redux/React-Native snippets" extension to help with React boilerplate code.
+  - `rafce` : Create a functional component
+
+#### Tailwind CSS Setup
+
+- Follow the instructions on: https://tailwindcss.com/docs/installation/using-vite
+-
---
+++ React/react_crash/src/App.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const App = () => {
+  //React components returns JSX and a JSX is a HTML like syntax which returns 1 parent element, insde there can be multiple child elements
+
+  const name = "Simon";
+  const age = 25;
+  const isLoggedIn = true;
+  const names = ["name1", "name2", "name3"];
+  const styles = {
+    color: "red",
+    fontSize: "16px",
+  };
+
+  return (
+    <>
+      <div className="text-5xl">App Test</div>
+      <p>
+        {/* conditional in JSX*/}
+        Hello {name}, your age is {age}. Is he logged in :
+        {isLoggedIn ? "affermative" : "negative"}
+      </p>
+      <p style={styles}>
+        {/* inline styling using tailwin*/}
+        Hello {name}
+      </p>
+      <ul>
+        {names.map(
+          (
+            index,
+            name //React uses keys to identify which elements have been changes, without keys React will re-render the entire list every time.
+          ) => (
+            <li key={index}>{name}</li>
+          )
+        )}
+      </ul>
+    </>
+  );
+};
+
+const MultipleChildren = () => {
+  return (
+    <>
+      <h1>Multiple Children</h1>
+      <h2>Hello</h2>
+      <h3>World</h3>
+    </>
+  );
+};
+
+export default App;
