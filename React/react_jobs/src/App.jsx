46bf906f83120fecfa7e2bf934486eead98663b0
Shaurya <shauryatomer10@gmail.com>
2025-04-06 20:42:26 +0530

    [feature] : Setup Navbar buttons to work dynamically and created JobsPage

--- React/react_jobs/src/App.jsx
+++ React/react_jobs/src/App.jsx
@@ -6,12 +6,14 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route path="/" element={<HomePage />} />
       {/*it creates a router to a path using the element h1 here, but it can be any component */}
+      <Route path="/jobs" element={<JobsPage />} />
     </Route>
   )
 );
